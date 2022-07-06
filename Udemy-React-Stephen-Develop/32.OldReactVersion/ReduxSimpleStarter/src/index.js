a6774f2c83baf1da6bca23ef358a07529740d460
Alberto Del Angel <rockstar4400@gmail.com>
2022-07-05 22:49:03 -0500

    Old code update

--- Udemy-React-Stephen-Develop/32.OldReactVersion/ReduxSimpleStarter/src/index.js
+++ Udemy-React-Stephen-Develop/32.OldReactVersion/ReduxSimpleStarter/src/index.js
@@ -1,15 +1,33 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDom from 'react-dom';
 import YTSearch from 'youtube-api-search';
 
 import SearchBar from './components/search_bar';
+import VideoList from './components/video_list';
 
-const API_KEY = 'AIzaSyCSBTsI650fbg-GeW8XKnTCt6LTuRBgjPs';
 
-const App = () => {
-    return (
-        <SearchBar />
-    );
+class App extends Component {
+
+    constructor(props){
+    super(props);
+
+    this.state = { videos: [] };
+
+    const API_KEY = 'AIzaSyCSBTsI650fbg-GeW8XKnTCt6LTuRBgjPs';
+
+    YTSearch({key: API_KEY, term: 'surfboards'}, (videos) => {
+        this.setState({videos});
+        });
+    }
+
+    render() {
+        return (
+            <div>
+                <SearchBar />
+                <VideoList videos={this.state.videos}/>
+            </div>
+        );
+    }
 }
 
 ReactDom.render(<App/>,document.querySelector('.container'));
