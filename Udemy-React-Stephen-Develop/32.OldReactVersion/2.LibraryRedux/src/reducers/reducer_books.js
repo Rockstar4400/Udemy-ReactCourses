2aa69a8baa4e6e14515e02282dd24cb888745106
Alberto Del Angel <rockstar4400@gmail.com>
2022-07-07 12:55:29 -0500

    LibraryProjectRedux

---
+++ Udemy-React-Stephen-Develop/32.OldReactVersion/2.LibraryRedux/src/reducers/reducer_books.js
@@ -0,0 +1,8 @@
+export default function(){
+    return [
+        {title: 'Javascript: The Good Parts'},
+        {title: 'Game of Thornes'},
+        {title: 'The dark tower'},
+        {title: 'Eloquent Ruby'},
+    ]
+}
